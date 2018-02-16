2131daac20421c34fd6bd7f60097fae77d8ee3d7
Ben Lee <leebenjamin@utexas.edu>
2018-02-16 12:01:01 -0600

    add css sheet and add server logic

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,23 @@ var http = require("http");
 var fs = require("fs");
 
 http.createServer(function (request, response) {
-    fs.readFile("src/index.html", function(err, data) {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);
-        response.end();
-    });
+    console.log(request.url);
+    if (request.url == "/" || request.url.indexOf(".html") != -1) { // request url contains html
+        fs.readFile("src/index.html", function (err, data) {
+            if (err) console.log(err);
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(data);
+            response.end();
+        });
+    }
+
+    else if (request.url.indexOf(".css") != -1) { // request url contains css
+        console.log("CSS");
+        fs.readFile("src/styles/style.css", function (err,data) {
+            if (err) console.log(err);
+            response.writeHead(200, {"Content-Type": "text/css"});
+            response.write(data);
+            response.end();
+        });
+    }
 }).listen(9000);
