0032f536200f47ae5e412ed4e767a3a077ccb450
Ben Lee <leebenjamin@utexas.edu>
2018-02-20 15:19:52 -0600

    add gulp tasks to start server and watch for changed files, favicon broken

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,41 @@
 var gulp = require("gulp");
+connect = require ("gulp-connect");
+
+gulp.task("dev", function() {
+    connect.server({
+        root: "app/",
+        port: 9000,
+        livereload: true
+    });
+});
 
 gulp.task("hello", function() {
     console.log("Hello, World!");
 });
+
+gulp.task("html", function () {
+  gulp.src("./app/*.html")
+    .pipe(gulp.dest("./app"))
+    .pipe(connect.reload());
+});
+
+gulp.task("css", function () {
+  gulp.src("./app/styles/*.css")
+    .pipe(gulp.dest("./app/styles"))
+    .pipe(connect.reload());
+});
+
+gulp.task("js", function () {
+  gulp.src("./app/js/*.js")
+    .pipe(gulp.dest("./app/js"))
+    .pipe(connect.reload());
+});
+
+gulp.task("watch", function () {
+  gulp.watch(["./app/*.html"], ["html"]);
+  gulp.watch(["./app/styles/*.css"], ["css"]);
+  gulp.watch(["./app/js/*.js"], ["js"]);
+});
+
+
+gulp.task("default", ["dev", "watch"]);
