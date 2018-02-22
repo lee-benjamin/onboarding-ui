8ac18bc0a1f5be6b6fcb4808175eafc895c62c15
Ben Lee <leebenjamin@utexas.edu>
2018-02-22 17:21:46 -0600

    convert gulpfile to use es6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,53 @@
 var gulp = require("gulp");
 var connect = require("gulp-connect");
 var sass = require("gulp-sass");
+const babel = require("gulp-babel");
 
-gulp.task("start-server", function() {
+gulp.task("babel", () =>
+    gulp.src("src/js/*.js")
+        .pipe(babel({
+            presets: ["es2017"]
+        }))
+        .pipe(gulp.dest("app/js/"))
+);
+
+gulp.task("start-server", () =>
     connect.server({
         root: "app/",
         port: 9000,
         livereload: true
-    });
-});
+    })
+);
 
-gulp.task("hello", function() {
-    console.log("Hello, World!");
-});
+gulp.task("hello", () =>
+    console.log("Hello, World!")
+);
 
-gulp.task("html", function () {
+gulp.task("html", () =>
   gulp.src("./src/*.html")
     .pipe(gulp.dest("./app"))
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+);
 
-gulp.task("sass", function() {
+gulp.task("sass", () =>
     gulp.src("./src/styles/sass/*.scss")
         .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("./app/styles/css"));
-});
+        .pipe(gulp.dest("./app/styles/css"))
+);
 
-gulp.task("js", function () {
+gulp.task("js", () =>
   gulp.src("./src/js/*.js")
     .pipe(gulp.dest("./app/js"))
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+);
 
-gulp.task("assets", function () {
+gulp.task("assets", () =>
    gulp.src("./src/assets/*.ico")
     .pipe(gulp.dest("./app/assets"))
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+);
 
-gulp.task("watch", function () {
+gulp.task("watch", () => {
   gulp.watch(["./src/*.html"], ["html"]);
   gulp.watch(["./src/styles/sass/*.scss"], ["sass"]);
   gulp.watch(["./src/js/*.js"], ["js"]);
