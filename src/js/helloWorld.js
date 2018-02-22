d2f693d8b55e19f04fb9174ca6add4f8e9bf4554
Ben Lee <leebenjamin@utexas.edu>
2018-02-22 15:42:05 -0600

    add event listener to avoid race condition on script loading order

--- src/js/helloWorld.js
+++ src/js/helloWorld.js
@@ -1,4 +1,6 @@
-ReactDOM.render(
+document.addEventListener("DOMContentLoaded", function(event) {
+  ReactDOM.render(
     React.createElement("div", null, "Hello, World!"),
     document.getElementById("reactComponent")
-);
+  );
+});
