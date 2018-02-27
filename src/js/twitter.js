d3cb1c67c57c5b9bff515658651ffa6c2fb862fa
Ben Lee <leebenjamin@utexas.edu>
2018-02-27 13:59:51 -0600

    convert tabs to 2 spaces

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -1,72 +1,72 @@
 const e = React.createElement; // syntatical shorthand
 
 function GetHomeTimelineButton() {
-    return e(
-        "button",
-        {onClick: getHomeTimeline},
-        "Get Home Timeline"
-    );
+  return e(
+    "button",
+    {onClick: getHomeTimeline},
+    "Get Home Timeline"
+  );
 }
 
 function UserInfo(props) {
-    return e(
-        "figcaption",
-        null,
-        e(
-            "div",
-            { className: "imageCaption" },
-            props.user.name,
-            e(
-                "div",
-                { className: "screenName" },
-                props.user.screenName
-            )
-        )
-    );
+  return e(
+    "figcaption",
+    null,
+    e(
+      "div",
+      { className: "imageCaption" },
+      props.user.name,
+      e(
+        "div",
+        { className: "screenName" },
+        props.user.screenName
+      )
+    )
+  );
 }
 
 function Avatar(props) {
-    return e(
-        "figure",
-        { className: "Avatar" },
-        e(
-            "img",
-            { className: "ProfilePic",
-                src: props.user.profileImageURL
-            }),
-        e(UserInfo, {user: props.user}),
-    );
+  return e(
+    "figure",
+    { className: "Avatar" },
+    e(
+      "img",
+      { className: "ProfilePic",
+        src: props.user.profileImageURL
+      }),
+    e(UserInfo, {user: props.user}),
+  );
 }
 
 function TweetContent(props) {
-    console.log("Tweet Content: " + props);
-    return e(
-        "div",
-        {className: "TweetContent"},
-        e("div", {className: "timeDiv"}, formatDate(props.tweet.createdAt)),
-        e("a", {href: getTwitterLink(props.tweet), target: "_blank"},
-            e("div", null, props.tweet.text)
-        )
-    );
+  console.log("Tweet Content: " + props);
+  return e(
+    "div",
+    {className: "TweetContent"},
+    e("div", {className: "timeDiv"}, formatDate(props.tweet.createdAt)),
+    e("a", {href: getTwitterLink(props.tweet), target: "_blank"},
+      e("div", null, props.tweet.text)
+    )
+  );
 }
 
 function TweetDiv(props) {
-    return e (
-        "div",
-        {className: "TweetDiv"},
-        e(Avatar, {user: props.user}),
-        e(TweetContent, {tweet: props}),
-    );
+  return e (
+    "div",
+    {className: "TweetDiv"},
+    e(Avatar, {user: props.user}),
+    e(TweetContent, {tweet: props}),
+  );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    ReactDOM.render(GetHomeTimelineButton(),
-            document.getElementById("getTimelineButton"));
-        getHomeTimeline();
+  ReactDOM.render(GetHomeTimelineButton(),
+	document.getElementById("getTimelineButton"));
+  getHomeTimeline();
 });
 
 const addImage = (tweetDiv, tweet) => {
-    // Append the image within a figure tag
+	// Append the image within a figure tag
     const figure = document.createElement("figure");
     figure.setAttribute("class", "Avatar");
     const figCaption = document.createElement("figcaption");
@@ -81,22 +81,22 @@ const addImage = (tweetDiv, tweet) => {
 }
 
 const formatDate = (input) => {
-    const date = new Date(input);
-    const monthNames = [
-        "January", "February", "March",
-        "April", "May", "June", "July",
-        "August", "September", "October",
-        "November", "December"
-    ];
+  const date = new Date(input);
+  const monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
 
-    const day = date.getDate();
-    const monthIndex = date.getMonth();
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
 
-    return monthNames[monthIndex] + " " + day;
+  return monthNames[monthIndex] + " " + day;
 }
 const getTwitterLink = (tweet) => {
-    const urlPrefix = "https://twitter.com/TwitterAPI/status/";
-    return urlPrefix + tweet.id;
+  const urlPrefix = "https://twitter.com/TwitterAPI/status/";
+  return urlPrefix + tweet.id;
 }
 
 const addTime = (tweetDiv, tweet) => {
@@ -140,29 +140,29 @@ const appendTweet = (tweet) => {
 }
 
 const getHomeTimeline = () => {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        const divTimeline = document.getElementById("divTimeline");
-        divTimeline.innerHTML = ""; // Clearing previous text
-        if (this.readyState == XMLHttpRequest.DONE)  {
-            if (this.status == 200) {
-                divTimeline.classList.add("divTimelineWithContent");
-                const obj = JSON.parse(this.responseText);
-                // only displaying one tweet for development purposes
-                let tweets = [];
-                for (let i=0; i<obj.length;i++) {
-                    tweets.push(TweetDiv(obj[i]));
-                }
-                ReactDOM.render(tweets,
-                    document.getElementById("divTimeline"));
-                    //obj.forEach((tweet) => {appendTweet(tweet)});
-            }
-            else {
-                divTimeline.classList.remove("divTimelineWithContent");
-                divTimeline.innerHTML = "Unable to get home timeline, please try again later.";
-            }
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    const divTimeline = document.getElementById("divTimeline");
+    divTimeline.innerHTML = ""; // Clearing previous text
+    if (this.readyState == XMLHttpRequest.DONE)  {
+      if (this.status == 200) {
+        divTimeline.classList.add("divTimelineWithContent");
+        const obj = JSON.parse(this.responseText);
+        // only displaying one tweet for development purposes
+        let tweets = [];
+        for (let i=0; i<obj.length;i++) {
+            tweets.push(TweetDiv(obj[i]));
         }
-    };
-    xhttp.open("GET", "http://localhost:8080/api/1.0/twitter/timeline",true);
-    xhttp.send();
+        ReactDOM.render(tweets,
+          document.getElementById("divTimeline"));
+          //obj.forEach((tweet) => {appendTweet(tweet)});
+      }
+      else {
+        divTimeline.classList.remove("divTimelineWithContent");
+        divTimeline.innerHTML = "Unable to get home timeline, please try again later.";
+      }
+    }
+  };
+  xhttp.open("GET", "http://localhost:8080/api/1.0/twitter/timeline",true);
+  xhttp.send();
 }
