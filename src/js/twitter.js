165249c3f431393120fbc47c0e4bab8633d2f914
Ben Lee <leebenjamin@utexas.edu>
2018-02-27 15:00:11 -0600

    unmount container for button

--- src/js/twitter.js
+++ src/js/twitter.js
@@ -1,9 +1,20 @@
 const e = React.createElement; // syntatical shorthand
 
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(GetHomeTimelineButton(),
+    document.getElementById("getTimelineButton"));
+  getHomeTimeline();
+});
+
 function GetHomeTimelineButton() {
+  function handleClick() {
+    ReactDOM.unmountComponentAtNode(document.getElementById("divTimeline"));
+    getHomeTimeline();
+  }
+
   return e(
     "button",
-    {onClick: getHomeTimeline},
+    {onClick: handleClick},
     "Get Home Timeline"
   );
 }
@@ -39,7 +50,6 @@ function Avatar(props) {
 }
 
 function TweetContent(props) {
-  console.log("Tweet Content: " + props);
   return e(
     "div",
     {className: "TweetContent"},
@@ -62,27 +72,6 @@ function TweetDiv(props) {
   );
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(GetHomeTimelineButton(),
-	document.getElementById("getTimelineButton"));
-  getHomeTimeline();
-});
-
-const addImage = (tweetDiv, tweet) => {
-	// Append the image within a figure tag
-    const figure = document.createElement("figure");
-    figure.setAttribute("class", "Avatar");
-    const figCaption = document.createElement("figcaption");
-    figCaption.innerHTML = "<div class='imageCaption'>" + tweet.user.screenName + "<div class='screenName'>" + tweet.user.name +"</div></div>";
-    const tweetImg = document.createElement("img");
-    tweetImg.setAttribute("class", "ProfilePic");
-    tweetImg.src = tweet.user.profileImageURL;
-
-    figure.appendChild(tweetImg);
-    figure.appendChild(figCaption);
-    tweetDiv.appendChild(figure);
-}
-
 const formatDate = (input) => {
   const date = new Date(input);
   const monthNames = [
@@ -102,46 +91,6 @@ const getTwitterLink = (tweet) => {
   return urlPrefix + tweet.id;
 }
 
-const addTime = (tweetDiv, tweet) => {
-    // Append the timestamp
-    const timeDiv = document.createElement("div");
-    timeDiv.setAttribute("class", "timeDiv");
-    const createdAt = document.createTextNode(formatDate(tweet.createdAt));
-    timeDiv.appendChild(createdAt);
-    tweetDiv.appendChild(timeDiv);
-}
-
-const addText = (tweetDiv, tweet) => {
-    // Link the div to Twitter
-    const a  = document.createElement("a");
-    const urlPrefix = "https://twitter.com/TwitterAPI/status/";
-    a.setAttribute("href", urlPrefix + tweet.id);
-    a.setAttribute("target", "_blank");
-
-    // Append the body of the tweet
-    const bodyDiv = document.createElement("div");
-    bodyDiv.innerHTML = tweet.text;
-    a.appendChild(bodyDiv);
-    tweetDiv.appendChild(a);
-}
-
-const appendTweet = (tweet) => {
-    // Each tweet will live in a <div>
-    const tweetDiv = document.createElement("div"); // The root element of the tweet
-    tweetDiv.classList.add("tweetDiv");
-
-    addImage(tweetDiv, tweet);
-    // create a div to hold the date and text
-    const tweetContent = document.createElement("div");
-    tweetContent.setAttribute("class", "tweetContent");
-    addTime(tweetContent, tweet);
-    addText(tweetContent, tweet);
-    tweetDiv.appendChild(tweetContent);
-    // Append the new tweet block to the DOM
-    const element = document.getElementById("divTimeline");
-    element.appendChild(tweetDiv);
-}
-
 const getHomeTimeline = () => {
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -150,14 +99,11 @@ const getHomeTimeline = () => {
     if (this.readyState == XMLHttpRequest.DONE)  {
       if (this.status == 200) {
         divTimeline.classList.add("divTimelineWithContent");
-        const obj = JSON.parse(this.responseText);
         let tweets = [];
-        for (let i=0; i<obj.length;i++) {
-            tweets.push(TweetDiv(obj[i]));
-        }
+        const obj = JSON.parse(this.responseText);
+        obj.forEach((tweet) => tweets.push(TweetDiv(tweet)));
         ReactDOM.render(tweets,
           document.getElementById("divTimeline"));
-          //obj.forEach((tweet) => {appendTweet(tweet)});
       }
       else {
         divTimeline.classList.remove("divTimelineWithContent");
