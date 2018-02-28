54a2f07408c375bcca29f9a06e5f9cfdc060f95c
Ben Lee <leebenjamin@utexas.edu>
2018-02-28 11:27:31 -0600

    render TimelineContainer in root, remove explicit html button

--- src/js/components.js
+++ src/js/components.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     getHomeTimeline( (tweets) => {
     console.log(tweets);
     ReactDOM.render(e(TimelineContainer, {tweets: tweets}),
-      document.getElementById("reactComponent"));
+      document.getElementById("root"));
   });
 });
 
